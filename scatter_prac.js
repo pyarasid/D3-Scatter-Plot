24e6e4168bf69eed98bb766a75a962d8823802bb
Siddharth Dixit <siddhart.dixit47@gmail.com>
2022-05-17 22:10:08 -0400

    updated code

--- scatter_prac.js
+++ scatter_prac.js
@@ -60,8 +60,83 @@ const dots= bound.selectAll("circle")
     .attr("cx", d=> xScale(xAccessor(d)))
     .attr("cy", d=> yScale(yAccessor(d)))
     .attr("r", 5)
-    .attr("fill", "darkgrey")
+    .attr("fill", "cornflowerblue")
 
+// Draw peripherals
+
+const xAxisGenerator= d3.axisBottom()
+    .scale(xScale)
+
+const xAxis = bound.append("g")
+    .call(xAxisGenerator)
+        .style("transform", `translateY(${dimensions.boundedheight}px)`)
+
+const xAxisLabel= xAxis.append("text")
+    .attr("x", dimensions.boundedwidth/2)
+    .attr("y", dimensions.margin.bottom-10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .html("Dew Point (&deg;F)")
+
+const yAxisGenerator = d3.axisLeft() 
+    .scale(yScale)
+    .ticks(4)      
+
+const yAxis = bound.append("g")
+    .call(yAxisGenerator)
+ 
+ const yAxisLabel= yAxis.append("text")
+    .attr("x", -dimensions.boundedheight/2) 
+    .attr("y", -dimensions.margin.left + 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .text("Relative humidity")
+    .style("transform", "rotate(-90deg)") 
+    .style("text-anchor", "middle") 
+
+// Interactions 
+
+
+bound.selectAll("circle")
+    .on("mouseenter", onMouseEnter)
+    .on("mouseleave", onMouseLeave)
+
+const tooltip= d3.select("#tooltip")
+
+function onMouseEnter(e, d){
+    
+    const formatdewpoint= d3.format(".2f")
+    tooltip.select("#dew-point")
+        .text(formatdewpoint(xAccessor(d)))
+    
+    const formathumidity= d3.format(".2f")
+    tooltip.select("#humidity")
+        .text(formathumidity(yAccessor(d)))
+        
+    const dateParser = d3.timeParse("%Y-%m-%d")
+    const formatDate= d3.timeFormat("%B %A %-d %Y")
+        
+    tooltip.select("#date")
+        .text(formatDate(dateParser(d.date)))
+
+    const x= xScale(xAccessor(d)) +
+        dimensions.margin.left
+    const y= yScale(yAccessor(d)) +
+        dimensions.margin.top
+
+    console.log(x)
+
+    tooltip.style("transform", `translate(`
+        + `calc(-50% + ${x}px),`   
+        + `calc(-100% + ${y}px)`
+        + `)` )
+
+    tooltip.style("opacity", 1)
+}
+
+function onMouseLeave(e, d){
+    tooltip.style("opacity", 0)
+}
 
 
 
